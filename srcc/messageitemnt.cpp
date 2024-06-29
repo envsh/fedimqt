0b4fcfc2515c7c803e2f3ce3333c6797c821340d
envsh <yatsen21@outlook.com>
2024-06-29 21:59:23 +0800

    som custom painter

--- srcc/messageitemnt.cpp
+++ srcc/messageitemnt.cpp
@@ -10,17 +10,66 @@
 auto MessageItemntQmlType = qmlRegisterType<MessageItemnt>("MessageItemnt", 1, 0, "MessageItemnt");
 
 MessageItemnt::~MessageItemnt() {
-    qDebug()<<__FUNCTION__<<__LINE__<<"dtor"<<this;
+    qDebug()<<__FUNCTION__<<__LINE__<<"dtor"<<this<<(sizeof(MessageItemnt));
 }
 
 MessageItemnt::MessageItemnt(QQuickItem* parent) : QQuickPaintedItem(parent) {
 
 }
 
+    QString MessageItemnt::GetContent() const { return this->Content; }
+    void MessageItemnt::SetContent(QString s) { this->Content = s; }
+    QString MessageItemnt::GetSender() const { return Sender; }
+    void MessageItemnt::SetSender(QString s) { Sender = s; }
+    QString MessageItemnt::GetRoomid() const { return Roomid; }
+    void MessageItemnt::SetRoomid(QString s) { Roomid = s; }
+    QString MessageItemnt::GetRoomname() const { return Roomname; }
+    void MessageItemnt::SetRoomname(QString s) { Roomname = s; }
+    QString MessageItemnt::GetEventid() const { return Eventid; }
+    void MessageItemnt::SetEventid(QString s) { Eventid = s; }
+    QString MessageItemnt::GetMtimemsui() const { return Mtimemsui; }
+    void MessageItemnt::SetMtimemsui(QString s) { Mtimemsui = s; }
+
+    int MessageItemnt::GetIndex() const { return Index; }
+    void MessageItemnt::SetIndex(int s) { Index = s; }
 
 void MessageItemnt::paint(QPainter *painter) {
-    QBrush brush(QColor("#007430"));
+    // QString Content = "hehehhe Content";
+    QString Roomname = "hehehhe Roomname";
 
+    QPen pen(Qt::white);
+    painter->setPen(pen);
+
+    QSizeF wsz = size();
+
+    painter->drawLine(0,1, wsz.width(), 1);
+    painter->drawLine(0, wsz.height(), wsz.width(), wsz.height());
+
+    painter->setOpacity(0.7);
+    painter->drawText(15,15, Sender);
+    painter->setOpacity(0.6);
+    painter->drawText(85,15, Roomname);
+    painter->setOpacity(0.5);
+    painter->drawText(165,15, Roomid);
+    painter->setOpacity(0.4);
+    painter->drawText(255,15, Mtimemsui);
+    
+    QRect rect0(0,0, wsz.width()-30, 30);
+    QRect ccrect = painter->boundingRect(rect0, Qt::TextWrapAnywhere, Content);
+    // qDebug()<<__FUNCTION__<<__LINE__<<ccrect;
+    if (height() < ccrect.height() + 50) {
+        this->setHeight(ccrect.height()+50);
+    }
+    
+    painter->setOpacity(0.9);
+    painter->drawText(15,35, wsz.width()-30, ccrect.height(), Qt::TextWrapAnywhere, Content);
+
+    // Content = "";
+    // Roomid = "";
+
+    return;
+
+    QBrush brush(QColor("#007430"));
     painter->setBrush(brush);
     painter->setPen(Qt::NoPen);
     painter->setRenderHint(QPainter::Antialiasing);
--- srcc/messageitemnt.h
+++ srcc/messageitemnt.h
@@ -15,7 +15,38 @@ class MessageItemnt : public QQuickPaintedItem
 {
     Q_OBJECT;
     // property heere
+
+    Q_PROPERTY(QString content READ GetContent WRITE SetContent FINAL);
+    Q_PROPERTY(QString sender READ GetSender WRITE SetSender FINAL);
+    Q_PROPERTY(QString roomid READ GetRoomid WRITE SetRoomid FINAL);
+    Q_PROPERTY(QString roomname READ GetRoomname WRITE SetRoomname FINAL);
+    Q_PROPERTY(QString eventid READ GetEventid WRITE SetEventid FINAL);
+    Q_PROPERTY(QString mtimemsui READ GetMtimemsui WRITE SetMtimemsui FINAL);
+        Q_PROPERTY(int index READ GetIndex WRITE SetIndex FINAL);
     QML_ELEMENT;
+public:
+    QString Content;
+    QString GetContent() const;
+    void SetContent(QString s);
+    QString Sender;
+    QString GetSender() const;
+    void SetSender(QString s);
+    QString Roomid;
+    QString GetRoomid() const;
+    void SetRoomid(QString s);
+    QString Roomname;
+    QString GetRoomname() const;
+    void SetRoomname(QString s);
+    QString Eventid;
+    QString GetEventid() const;
+    void SetEventid(QString s);
+    QString Mtimemsui;
+    QString GetMtimemsui() const;
+    void SetMtimemsui(QString s);
+
+    int Index;
+    int GetIndex() const;
+    void SetIndex(int s);
 
 public:
     MessageItemnt(QQuickItem* parent = nullptr);
